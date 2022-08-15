af1ff8c88b398c2a3136da90a4f8da6a410c6753
Vadim Belyakhovich <vadik123belyahovich@gmail.com>
2022-08-15 11:01:08 +0300

    Initialize project with Spring(back-end) and React(front-end)

---
+++ cafiteria_back/src/main/java/by/belyahovich/cafiteria_back/CafiteriaBackApplication.java
@@ -0,0 +1,13 @@
+package by.belyahovich.cafiteria_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafiteriaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafiteriaBackApplication.class, args);
+    }
+
+}
---
+++ cafiteria_back/src/test/java/by/belyahovich/cafiteria_back/CafiteriaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package by.belyahovich.cafiteria_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafiteriaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
